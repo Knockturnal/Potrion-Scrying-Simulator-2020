6259afc7e5bacff6609530fca0044fb83047e6aa
Oivin <oivinx@gmail.com>
2020-04-16 23:10:27 +0200

    Upload Unity Project

---
+++ Assets/Scripts/Diviner.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System.Linq;
+using System.IO;
+
+public enum PotionType {Red, White, Blue, Green, Pink, Orange, Black};
+
+public class Diviner : MonoBehaviour
+{
+	public static Diviner myDiviner;
+	[SerializeField]
+	private List<DivinationResult> divinationResults;
+	[SerializeField]
+	private TextMeshProUGUI result;
+
+	private PotionType firstPotion;
+	private bool second;
+	private string CSVContent;
+	private void Awake()
+	{
+		myDiviner = this;
+		List<DivinationResult> divinationResults = new List<DivinationResult>();
+		ParseResultsCSV();
+	}
+
+	public void PotionSelected(PotionType type) 
+	{
+		if(second == false) 
+		{
+			firstPotion = type;
+			second = true;
+		}
+		else
+		{
+			List<DivinationResult> firstMatch = divinationResults.FindAll(toCheck => toCheck.first == firstPotion);
+			DivinationResult secondMatch = firstMatch.Find(toCheck => toCheck.second == type);
+			result.text = secondMatch.result;
+			second = false;
+		}
+	}
+
+	private void ParseResultsCSV()
+	{
+		CSVContent = File.ReadAllText(Application.streamingAssetsPath + "/results.csv");
+		string[] lines = CSVContent.Split('\n');
+		for (int i = 1; i < lines.Length; i++)
+		{
+			string[] columns = SplitCsvLine(lines[i]);
+
+			for (int j = 1; j < columns.Length - 1; j++)
+			{
+				DivinationResult currentResult = new DivinationResult();
+				currentResult.first = (PotionType)(j - 1);
+				currentResult.second = (PotionType)(i - 1);
+				currentResult.result = columns[j];
+				divinationResults.Add(currentResult);
+			}
+		}
+	}
+
+	/// <summary>
+	/// Function for parsing a single line of CSV data
+	/// </summary>
+	public static string[] SplitCsvLine(string line)
+	{
+		return (from System.Text.RegularExpressions.Match m in System.Text.RegularExpressions.Regex.Matches(line,
+		@"(((?<x>(?=[,\r\n]+))|""(?<x>([^""]|"""")+)""|(?<x>[^,\r\n]+)),?)",
+		System.Text.RegularExpressions.RegexOptions.ExplicitCapture)
+				select m.Groups[1].Value).ToArray();
+	}
+}
+
+[System.Serializable]
+public struct DivinationResult 
+{
+	public PotionType first;
+	public PotionType second;
+	[Multiline]
+	public string result;
+}
---
+++ Assets/Scripts/PotionButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotionButton : MonoBehaviour
+{
+	[SerializeField]
+	private PotionType type;
+
+	public void IWasClicked() 
+	{
+		Diviner.myDiviner.PotionSelected(type);
+	}
+}
